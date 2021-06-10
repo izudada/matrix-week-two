b8d3bd2582eb56c86f05918e2f463ee0d2fadf93
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-06-10 04:06:14 +0000

    Done with task3

--- src/task1.py
+++ src/task1.py
@@ -14,29 +14,58 @@ class PrimeIterator:
 		return self
 
 	def __next__(self):
-		self.count += 1	
-		if self.count <= self.n:
-				if self.count % 2 != 0:
+		self.count += 1		# Increment count which serves as an iteration
+		if self.count <= self.n:	#	Check when to stop iteration
+				if self.count % 2 != 0 and self.count % 3 != 0:		# Modelling the conditions for a prime number
 						result = self.count
 						return result
+				else:
+    					return " "
 		else:
     			raise StopIteration("End of iteration")
+				
 
-
+#	Accept n as input
 num = int(input("Enter stop value/number:"))
 if num < 1:
     	raise ValueError("You must enter number greater than 1")
 
-prime_numbers = PrimeIterator(num)
-for i in prime_numbers:
-		print(next(prime_numbers))
+
+prime_numbers = PrimeIterator(num)#
+# for i in prime_numbers:	#	Printing out the prime numbers using forloop
+# 		try:
+# 			print(next(prime_numbers))
+# 		except StopIteration:
+# 			print("End of iteration")
+while True:
+    	print(next(prime_numbers))
 
 
 
-
-
+# *********************************************************************************************************************************************
 
 # Generator for prime numbers
 def prime_generator(n):
-    	pass
+	"""
+		generates prime numbers from `1` to a number `n`,
+		where `n` is greater than 1 
+	"""
+	count = 2
+	num_list = iter(range(2,n))
+
+	while True:
+		element = next(num_list)
+		if count < n:
+			if int(element) % 2 != 0 and int(element) % 3 != 0:
+				print(count, element)
+				return element
+				count += 1
+			else:
+    				return ""
+		raise StopIteration("End of iteration")
+
+# result = prime_generator(num)
+
+# print(result)
+# print(result)
 
---
+++ src/task3.py
@@ -0,0 +1,39 @@
+class Animal:
+
+    legs = 4
+
+    def __init__(self, name):
+        self.name = name
+
+    def identity(self):
+        return self.name
+    
+    def has_legs(self):
+        return f"{self.name} has {4} legs"
+
+    def can_reproduce(self):
+        return "yes"
+
+
+class Dog(Animal):
+
+    def __init__(self, name):
+        self.name = name
+
+    def makes_sound(self):
+        return "whoof..."
+
+    def pack_member(self):
+        return "yes"
+
+
+class Cat(Animal):
+
+    def __init__(self, name):
+        self.name = name
+    
+    def makes_sound(self):
+        return "meow..."
+
+    def solitaty_member(self):
+        return "yes"
